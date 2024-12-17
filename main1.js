0a59226176aed60f6cd14cba6c0cd69c7240406b
Smiley876 <anzelged16@gmail.com>
2024-12-17 09:35:39 +0200

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,23 @@
+//addition
+let num1=5;
+let num2=6;
+let sum=num1+num2;
+document.getElementById(`result`).innerHTML="The total sum is:"+sum;
+
+//substraction
+var num3 = 9; 
+var num4 = 8;
+var sum1 = num3-num4;
+document.getElementById(`sub`).innerHTML=`The total is:${sum1}`;
+
+//multiplication
+var num5 = 9;
+var num6 = 8;
+var sum2 = num5 * num6;
+document.getElementById(`result2`).innerHTML =`The totals is ${sum2}`; 
+
+//division
+var num7 = 9;
+var num8 = 3;
+var sum3 = num7 / num8;
+document.getElementById(`div`).innerHTML =`The total is: ${sum3}`;
